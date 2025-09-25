718bfcac65c0625aac67dedb65a6ebb03361055c
kingdomdt21 <116419736+kingdomdt21@users.noreply.github.com>
2025-09-25 15:32:35 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,57 @@
-window.onscroll = function() {
-    var btn = document.getElementById("scrollTopBtn");
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-      btn.style.display = "block";
-    } else {
-      btn.style.display = "none";
-    }
-  };
+const rv = document.getElementById('review');
 
-  // Cuộn lên đầu khi nhấn nút
-  function scrollToTop() {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    });
+window.onscroll = function () {
+  var btn = document.getElementById("scrollTopBtn");
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    btn.style.display = "block";
+    rv.style.bottom = "90px"
+  } else {
+    btn.style.display = "none";
+    rv.style.bottom = "20px"
   }
+};
+
+// Cuộn lên đầu khi nhấn nút
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const floatElements = document.querySelectorAll(".float-in");
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+          observer.unobserve(entry.target);
+        }
+      });
+    },
+    {
+      threshold: 0.1,
+    }
+  );
+
+  floatElements.forEach((el) => observer.observe(el));
+});
+
+rv.addEventListener("mouseover",()=>{
+    rv.textContent="⭐ Đánh giá ở đây nhé";
+});
+rv.addEventListener("mouseout",()=>{
+    rv.textContent="⭐⭐⭐";
+});
+
+rv.addEventListener("touchstart",()=>{
+    rv.textContent="⭐ Đánh giá ở đây nhé";
+});
+rv.addEventListener("touchend",()=>{
+    rv.textContent="⭐⭐⭐";
+});
